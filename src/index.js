cb23a7abe8a665997a2cd453b806b2eb317337a3
Veranikl <nijakavata@gmail.com>
2023-04-21 17:53:15 +0200

    Updated JS for forecast

--- src/index.js
+++ src/index.js
@@ -27,6 +27,15 @@ function weatherData(response) {
     .setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "d1a86552de255334f6117b348c4519bd";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast() {
